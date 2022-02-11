b9fdb4f3fe16817fcaca3426cdf161913b446997
Brian Ferrari <brianf1996@knights.ucf.edu>
2022-02-11 16:36:47 +0100

    first commit

---
+++ contracts/interfaces/IStablecoin.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+interface IStablecoin {
+  function getDebtCeiling() external view returns (uint256);
+
+  function getClosingFee() external view returns (uint256);
+  
+  function getOpeningFee() external view returns (uint256);
+
+  function getTokenPriceSource() external view returns (uint256);
+
+  function getEthPriceSource() external view returns (uint256);
+
+  function createVault() external returns (uint256);
+
+  function destroyVault(uint256 vaultID) external;
+
+  function transferVault(uint256 vaultID, address to) external;
+
+  function depositCollateral(uint256 vaultID) external payable;
+
+  function withdrawCollateral(uint256 vaultID, uint256 amount) external;
+
+  function borrowToken(uint256 vaultID, uint256 amount) external;
+
+  function payBackToken(uint256 vaultID, uint256 amount) external;
+
+  function buyRiskyVault(uint256 vaultID) external;
+
+}
